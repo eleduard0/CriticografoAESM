09f745c7b5b20a2adcfe352ec66e36af28a74403
eleduard0 <arturo.13040570@gmail.com>
2024-01-11 13:29:24 -0700

    Agregar archivos de proyecto.

---
+++ CriticografoAESM/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CriticografoAESM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string mensaje;
+            string genero;
+            if (Hombre.IsChecked== true)
+            {
+                genero = "hombre";
+            }
+            else { genero = "mujer"; }
+
+            
+
+            Mensaje.Text = $"{Nombre.Text} es {genero}";
+        }
+    }
+}
